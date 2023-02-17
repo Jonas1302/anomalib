9c763a66e44b94ae28ee87f840503a39dd587673
Jonas Bühler <Jones132@posteo.net>
2023-02-17 10:46:09 +0100

    improve feature visualization

--- anomalib/post_processing/feature_visualizer.py
+++ anomalib/post_processing/feature_visualizer.py
@@ -1,9 +1,6 @@
 import colorsys
-import pickle
-from itertools import zip_longest
-
 import math
-from typing import Union, Dict, Tuple, List, Optional
+from typing import Union, Dict, Tuple, List, Any
 
 import numpy as np
 import torch
@@ -16,110 +13,116 @@ from sklearn.manifold import TSNE
 
 # for umap tutorial see https://umap-learn.readthedocs.io/en/latest/basic_usage.html
 
+class VisualizerEntry:
+    def __init__(self, labels: Union[str, List[str]], features: np.ndarray, values: Union[int, float, np.ndarray]):
+        if isinstance(features, torch.Tensor):
+            features = features.detach().cpu().numpy()
+        if len(features.shape) == 1:  # make sure all features have two dimensions because we'll concatenate them
+            features = np.expand_dims(features, 0)
+        assert isinstance(labels, str) or len(labels) == len(features)
+        assert isinstance(values, (int, float)) or len(values) == len(features)
+
+        self.labels = labels
+        self.features = features
+        self.values = values
+
+    def content(self):
+        return self.labels, self.features, self.values
+
+    @property
+    def expanded_labels(self):
+        return self.labels if isinstance(self.labels, (list, np.ndarray)) else np.full(len(self.features), self.labels)
+
+    @property
+    def expanded_values(self):
+        return self.values if isinstance(self.values, np.ndarray) else np.full(len(self.features), self.values)
+
+    @staticmethod
+    def merge(*instances):
+        labels, features, values = zip(*map(VisualizerEntry.content, instances))
+        expand_labels = any([isinstance(l, list) or l != labels[0] for l in labels])
+        expand_values = any([isinstance(v, np.ndarray) or v != values[0] for v in values])
+        new_labels = np.concatenate([e.expanded_labels for e in instances]) if expand_labels else labels[0]
+        new_values = np.concatenate([e.expanded_values for e in instances]) if expand_values else values[0]
+        new_features = np.concatenate(features)
+
+        assert isinstance(new_labels, str) or len(new_labels) == len(new_features)
+        assert isinstance(new_values, (int, float)) or len(new_values) == len(new_features)
+        return VisualizerEntry(new_labels, new_features, new_values)
+
+    @classmethod
+    def create_by_label(cls, labels: List[str], features: np.ndarray, values: np.ndarray) -> "Dict[Any, VisualizerEntry]":
+        assert len(labels) == len(features) == len(values)
+        result = {}
+        grouped_features = {}
+        grouped_values = {}
+        for l, f, v in zip(labels, features, values):
+            grouped_features.setdefault(l, []).append(f)
+            grouped_values.setdefault(l, []).append(v)
+        for l in grouped_features:
+            result[l] = VisualizerEntry(l, np.stack(grouped_features[l]), np.stack(grouped_values[l]))
+        return result
+
+
 class FeatureVisualizer:
     def __init__(self, *reducers):
         self.reducers = reducers
         self.reducer_uptodate = True
-        self.features: Dict[str, List[np.ndarray]] = {}
-        self.values: Dict[str, List[np.ndarray]] = {}
+        self.entries: List[VisualizerEntry] = []
 
-    def add_features(self, features: Union[np.ndarray, torch.Tensor], label: Union[str, List[str], np.ndarray] = 0,
-                     values: Optional[Union[float, np.ndarray]] = None):
+    def add_features(self, features: Union[np.ndarray, torch.Tensor], label: Union[str, List[str]] = 0,
+                     values: Union[float, np.ndarray] = 1):
         """Add features to be visualized. Features with the same `label` will have the same color."""
-        assert self.values is None or values is not None, "either all features must have values or none of them"
+        self.entries.append(VisualizerEntry(label, features, values))
+        self.reducer_uptodate = False
 
-        if isinstance(features, torch.Tensor):
-            features = features.detach().cpu().numpy()
-        if len(features.shape) == 1:  # make sure all features have two dimensions because we'll concatenate them
-            features = [features]
-
-
-        if isinstance(label, str):
-            self.features.setdefault(label, []).append(features)
-            if values is not None:
-                if isinstance(values, (int, float)):
-                    values = [values]
-                self.values.setdefault(label, []).append(values)
-            self.reducer_uptodate = False
-        else:  # recursive
-            assert len(label.shape) == 1, "category must either be `int` or an one-dimensional array"
-            assert len(values) == len(features), f"{len(values)} != {len(features)}"
-
-            for i in range(len(label)):
-                self.add_features(features[i], label[i], values[i] if isinstance(values, (list, np.ndarray)) else values)
-
-    def _sorted_flattened_coordinates_and_labels(self) -> Dict[str, Tuple[np.ndarray, np.ndarray]]:
+    def _sorted_flattened_coordinates_and_labels(self) -> Dict[str, VisualizerEntry]:
         # the value entries are currently list of two-dimensional arrays
         # => concatenate them so that we have one numpy array per label
-        concatenated_features = {label: np.concatenate(features_list) for label, features_list in self.features.items()}
+        entries_by_label = VisualizerEntry.create_by_label(*VisualizerEntry.merge(*self.entries).content())
         # sort coordinates and labels such that labels with more points are drawn first
         # (and thus rare labels are shown on top of them in case they overlap)
-        if self.values:
-            concatenated_values = {label: np.concatenate(values_list) for label, values_list in self.values.items()}
-            tuples = [(l, f, v) for (l, f), v in zip(concatenated_features.items(), concatenated_values.values())]
-            labels, sorted_features, values = zip(*sorted(tuples, key=lambda x: len(x[1]), reverse=True))
-        else:
-            labels, sorted_features = zip(*sorted(concatenated_features.items(), key=lambda x: len(x[1]), reverse=True))
-            values = []
-
-        # create a single two-dimensional array of all features
-        flattened_features = np.concatenate(sorted_features)
-        # same for values
-        if values:
-            values = np.concatenate(values)
-        # create a verbose array of labels with one entry per coordinate (like [1, 1, 1, 0, 0, 2] for 6 coordinates and 3 labels)
-        labels = np.concatenate([np.full(len(concatenated_features[label]), label) for label in labels])
+        entries_by_label = {l: e for l, e in sorted(entries_by_label.items(), key=lambda i: sum(i[1].expanded_values), reverse=True)}
 
         # apply dimensionality reductions
-        coordinates = flattened_features
+        concatenated_entries = VisualizerEntry.merge(*entries_by_label.values())
+        coordinates = concatenated_entries.features
         for reducer in self.reducers:
             coordinates = reducer.fit_transform(coordinates)
-        result = {}
-        for c, l, v in zip_longest(coordinates, labels, values):
-            result.setdefault(l, []).append((c, v))
-        for l in result:
-            features, values = zip(*result[l])
-            result[l] = (np.stack(features), np.stack(values))
-        return result
+
+        return VisualizerEntry.create_by_label(concatenated_entries.labels, coordinates, concatenated_entries.values)
 
     def get_pyplot(self, **pyplot_args) -> plt.Figure:
         """Process all features and returns a pyplot figure."""
         coordinates = self._sorted_flattened_coordinates_and_labels()
 
         # generate point colors
-        color_mapper = ColorMapper(list(self.features.keys()))  # use `self.features` instead of `coordinates` to keep the order of the labels
-        #print(color_mapper.labels)
-        #print(color_mapper.nested_labels)
-        #print({l: colorsys.rgb_to_hsv(*color_mapper.get_color(l)) for l in color_mapper.labels})
-        #print({l: color_mapper.get_color(l) for l in color_mapper.labels})
-        #categories = {k: LinearSegmentedColormap.from_list("", ["white", v]) for k, v in categories.items()}
+        color_mapper = ColorMapper(list(sorted(coordinates.keys())))
 
         # create the plot
-        max_marker_size = 1
+        max_marker_size = 2
         fig = plt.Figure(figsize=(19.20 / 2, 10.80 / 2), dpi=800)
         axes = fig.gca()
         # plot points
         # TODO maybe use axes.contour? (https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.contour.html)
-        for label in coordinates:
-            c, values = coordinates[label]
-            if values[0] is None:
-                pyplot_args['color'] = color_mapper.get_color(label)
-                pyplot_args['s'] = max_marker_size / max(math.log(len(c) / 10, 100), 1)
-            else:
-                pyplot_args['c'] = values
-                pyplot_args['cmap'] = color_mapper.get_colormap(label)
-                pyplot_args['s'] = max_marker_size / max(math.log(sum(values) / 10, 100), 1)
-            print(len(c), max_marker_size / max(math.log(len(c) / 10, 100), 1), pyplot_args['s'])
-
+        for label, entry in coordinates.items():
+            print(label, len(entry.expanded_values), sum(entry.expanded_values), max_marker_size / max(math.log(sum(entry.expanded_values), 100), 1))
             axes.scatter(
-                c[:, 0],
-                c[:, 1],
+                entry.features[:, 0],
+                entry.features[:, 1],
                 linewidths=0,
                 label=label,
+                c=entry.expanded_values,
+                cmap=color_mapper.get_colormap(label),
+                s=max_marker_size / max(math.log(sum(entry.expanded_values), 100), 1),
+                vmin=0,
+                vmax=1,
                 **pyplot_args)
 
         # create legend
-        legend = axes.legend(fontsize='xx-small', markerscale=5/max_marker_size)
+        handles, labels = axes.get_legend_handles_labels()
+        handles, labels = list(zip(*sorted(zip(handles, labels), key=lambda t: t[1])))
+        legend = axes.legend(handles, labels, fontsize='xx-small', markerscale=6/max_marker_size)
         for lh in legend.legendHandles:  # from https://stackoverflow.com/a/42403471
             lh.set_alpha(1)
 
@@ -134,12 +137,12 @@ class FeatureVisualizerUMAP(FeatureVisualizer):
         super().__init__(umap.UMAP())
 
     def get_pyplot(self, **pyplot_args) -> plt.Figure:
-        coordinates, labels = self._sorted_flattened_coordinates_and_labels()
+        all_entries = VisualizerEntry.merge(*self._sorted_flattened_coordinates_and_labels().values())
 
-        fig = plt.Figure(figsize=(5, 5), dpi=800)
+        fig = plt.Figure(figsize=(19.20, 10.80), dpi=400)
         ax = umap.plot.points(self.reducers[-1],
-                              points=coordinates,
-                              labels=labels,
+                              points=all_entries.features,
+                              labels=all_entries.expanded_labels,
                               theme="inferno",
                               ax=plt.Axes(fig, (0., 0., 1., 1.)))
         fig.add_axes(ax)
